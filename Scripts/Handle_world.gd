34e836a5bceef890bf766c896951e55dfa46fa2b
SKAIJU <axkugelmann@gmail.com>
2023-08-25 15:02:15 +0200

    chunk loading semi complete

--- Scripts/Handle_world.gd
+++ Scripts/Handle_world.gd
@@ -4,7 +4,7 @@ extends Node3D
 var chunk: PackedScene = preload("res://Scenes/Terrain_chunk.tscn")
 var player: Node3D
 var chunk_size: float = 20
-var render_distance: float = 2
+var render_distance: float = 1
 var chunks:= {}
 var current_chunk: Vector2 = Vector2(200,200)
 var updated_chunk = current_chunk
@@ -14,42 +14,64 @@ var offset = int(-grid_size/2)
 
 func _ready():
 	player = $Player
-	updated_chunk = Vector2(int(player.position.x/chunk_size),int(player.position.z/chunk_size))	
-	initialize_chunks()
-	
-
-func check_current_chunk():
+	await get_tree().create_timer(1.0).timeout		
 	updated_chunk = Vector2(int(player.position.x/chunk_size),int(player.position.z/chunk_size))
-	if current_chunk != updated_chunk:
-		handle_chunks()
-		print(chunks.size())
-		current_chunk = updated_chunk
-	
+	initialize_chunks()
+	print(chunks.size())
+	current_chunk = updated_chunk	
+	check_current_chunk()
+
+
 func initialize_chunks():
 	for z in grid_size:
 		for x in grid_size:
 			
-			var this_chunk = chunk.instantiate()
 			var chunk_pos = updated_chunk + Vector2(x + offset,z + offset)
 			
+			var this_chunk = chunk.instantiate()
 			this_chunk.position = Vector3(chunk_pos.x, 0, chunk_pos.y) * (chunk_size - 1)
 			add_child(this_chunk)
 			chunks[chunk_pos] = this_chunk
+			
+			
+func check_current_chunk():
+	while true:	
+		updated_chunk = Vector2(int(player.position.x/chunk_size),int(player.position.z/chunk_size))
+
+		if current_chunk != updated_chunk:
+			handle_chunks()
+			print(chunks.size())
+			current_chunk = updated_chunk
+			
+		await get_tree().create_timer(1).timeout		
+		
+	
 
 
 func handle_chunks():
+	var old_chunks = []
+	var count = 0
+	for chunk_pos in chunks:
+		var distance = Vector2(abs(chunk_pos.x - updated_chunk.x), abs(chunk_pos.y - updated_chunk.y))
+		print("dist: ", distance)
+		if distance.x >= render_distance or distance.y >= render_distance:
+			old_chunks.append(chunks[chunk_pos])
+			chunks.erase(chunk_pos)
+			count+=1
+	print("updated: ",count," cells")
+	
 	for z in grid_size:
 		for x in grid_size:
 			
 			var chunk_pos = updated_chunk + Vector2(x + offset,z + offset)
-				
 			if !chunks.has(chunk_pos):
-				var this_chunk = chunk.instantiate()
-				this_chunk.position = Vector3(chunk_pos.x, 0, chunk_pos.y) * (chunk_size - 1)
-				add_child(this_chunk)
-				chunks[chunk_pos] = this_chunk
+				if old_chunks.is_empty(): break
+				chunks[chunk_pos] = old_chunks[0] 	
+				old_chunks.remove_at(0)	
+				chunks[chunk_pos].position = Vector3(chunk_pos.x, 0, chunk_pos.y) * (chunk_size - 1)
+				chunks[chunk_pos]._ready()
+				
+			
 
 
-func _process(delta):
-	check_current_chunk()
-	await get_tree().create_timer(1.0).timeout
+

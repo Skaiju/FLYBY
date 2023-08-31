9cf95285b5ec4275b5b6a68050d38fe29849b264
SKAIJU <axkugelmann@gmail.com>
2023-08-31 02:48:51 +0200

    added jhusss plane controller

--- Scripts/Cam_controller.gd
+++ Scripts/Cam_controller.gd
@@ -1,16 +1,18 @@
 extends Camera3D
 
-@onready var camera_points: Array[Node3D] = [$"../plane/FirstPerson", $"../ThirdPerson"]
+@onready var camera_points: Array[Node3D] = [$"../ThirdPerson", $"../plane/FirstPerson", $"../plane/FirstPerson2"]
 
 var active_camera_mount: Node3D
-var active_camera_index: int = 1
-
+var active_camera_index: int = 0
+var smooth:float = 0.7
 func _ready():
 	active_camera_mount = camera_points[active_camera_index]
 
 func _physics_process(delta):
 	global_position = lerp(global_position, active_camera_mount.global_position, 0.15)
-	global_rotation = lerp(global_rotation, active_camera_mount.global_rotation, 0.15)
+	global_rotation.x = lerp_angle(global_rotation.x, active_camera_mount.global_rotation.x, smooth)
+	global_rotation.y = lerp_angle(global_rotation.y, active_camera_mount.global_rotation.y, smooth)
+	global_rotation.z = lerp_angle(global_rotation.z, active_camera_mount.global_rotation.z, smooth)
 
 func _input(event):
 	if Input.is_action_just_pressed("change_camera"):
--- Scripts/Plane_controller.gd
+++ Scripts/Plane_controller.gd
@@ -1,13 +1,47 @@
 extends CharacterBody3D
 
 const SENSITIVITY: float = 1
-const SPEED: float = 15
+const SPEED: float = 1000
 
 @export var roll: float = 0
 @export var pitch: float = 0
-var roll_strength: float = 0
-var pitch_strength: float = 0
+var roll_dir: float = 0
+var pitch_dir: float = 0
 var roll_angle: float = 80
+var roll_vel: float = 8
+var pitch_vel: float = 3
+var smooth:float = 0.05
+var omega_roll:float = deg_to_rad(120)
+var omega_pitch:float = deg_to_rad(80)
+var roll_str = 0
+var pitch_str = 0
+
 @onready var plane = $plane
+	
+func smooth_acc(strength:float, delta:float, omega:float):
+	return sin(strength * PI/2) * delta * omega
 
-
+func increment_vel(strength:float,dir:float):
+	strength += smooth if dir > 0 else -smooth if dir < 0 else -sign(strength)*min(smooth,abs(strength))
+	return clampf(strength,-1.0,1.0)
+	
+func _physics_process(delta):
+	roll_dir = Input.get_axis("roll_left", "roll_right")
+	pitch_dir = Input.get_axis("pitch_up", "pitch_down")
+	
+	
+	pitch_str = increment_vel(pitch_str,pitch_dir)
+	roll_str = increment_vel(roll_str,roll_dir)
+	
+	var roll = smooth_acc(roll_str,delta,omega_roll)
+	var pitch = smooth_acc(pitch_str,delta,omega_pitch)
+	
+	rotate_object_local(Vector3.FORWARD,-roll)
+	rotate_object_local(Vector3.RIGHT, pitch)
+	#rotate_object_local(Vector3.RIGHT,pitch)
+#	rotate_object_local(Vector3.RIGHT,lerpf(rotation.z, -pitch, smooth))
+	
+#	local_rotation.z = lerpf(local_rotation.z, roll, smooth)
+#	local_rotation.x = lerpf(local_rotation.x, pitch, smooth)
+	velocity = transform.basis.z  * SPEED * delta
+	move_and_slide()
